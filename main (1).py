d7876443e173839f34daae129eb8fbf386f97ca1
Kathirvelk139 <144417378+Kathirvelk139@users.noreply.github.com>
2023-09-15 13:34:54 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,24 +1,32 @@
-#1.1 implement a recursive function to celeculate the factotial of a given number.
-
-"""
-1! = 1 * 1
-2! = 2 * 1! --->2 * 1
-3! = 3 * 2! --->3 * 2 * 1
-.
-.
-10! = 10 * 9! ---> 10 * 9 * 8 *... * 1
-
-Formula - n * (n-1)1!
-"""
+''' Implement a class called Player that represents a cricket player.
+The Player class should have a
+method called play() which prints "The player is playing cricket.
+Derive two classes, Batsamn and 
+Bowler, from the player class. Override the play() method in each
+derived class to print "The batsman
+is battting" and "The bowler is bowling", respectively. Write a program to create objects of both the 
+Batsman and Bowler classes and call the play() method for each 
+object.'''
 
 
-def fact_rec(n):
-  if n==0 or n==1:
-     return 1
-  else:
-     return n*fact_rec(n-1)
-number = int(input("Enter a value : "))
-res = fact_rec(number)
+# Define the base class Player
+class Player:
+  def play(self):
+    print("The player is playing cricket.")
 
+# Define the derived class Bastman
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
 
-print(" The factorial of {}.".format(number,res))
+# Define the derived class Bowler
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
+# Create objects of Batsman ans Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+#Call the play() method for each object
+batsman.play()
+bowler.play()
--- main (2).py
+++ main (2).py
@@ -1,21 +1,48 @@
-# Leap year 
-"""
-year % 4 == 0 &
-year % 100 != 0 /
-year % 400 == 0
+''' Implement a class called Bank account that represents a bank account. The class should have private
+attributes for account number, account holder name, and account balance. Include methods to
+deposit money, withdraw monye, and display the account balance. Ensure that the account balance
+cannot be accessed directly form outside the class. write a program to create an instance of the
+BankAccount class and test the deposit and withdrawal functionality.'''
 
 
-"""
-def isLeapYear(year):
-  if (year % 4 == 0 and year % 100 != 0) or year % 400 ==0:
-    return  True 
+class BankAccount:
+
+  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+    self.__account_number = account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance = initial_balance
+
+def deposit(self, amount):
+  if amount > 0:
+    self.__account_balance += amount 
+    # self.__account_balance =  self.__account_balance+amount
+    print("Deposited ₹{}. New balance: ₹{}".format(amount, 
+                                                   self.__account_balance))
   else:
-    return False 
+    print("Invalid deposit amount.please deposit a positive amount.")
 
-year = int(input())
+def withdraw(self, amount):
+  if amount > 0 and amount <= self.__account_balance:
+    self.__account_balance -= amount
+    # self.__account_balance = self.__account_balancel - amount
+    print("withdrew ₹{}. New balance: ₹{}".format(amount, 
+                                                  self.__account_balance))
+  else:
+    print("Invalid withdrawal amount or insufficient balance.")
 
-if isLeapYear(year):
-  print('{} is a leap year.'.format(year))
-else:
-  print('{} is not a leap year.'.format(year))
-  
+def display_balance(srlf):
+  print("Account balance for {} (Account #{}): ₹}".format(
+     self.__account_holder_name,         self.__account_number,
+     self.__account_balance))
+
+
+#creat an instance of the BankAccount class
+account= BankAccount(account_number="123456789",
+                     account_holder_name="Hari prabu",
+                               initial_balance=5000.0)
+
+# Test deposit and withdrawal functionality
+#account.display_balance()
+#account.deposit(500.0)
+#account.withdraw(200.0)
+#account.display_balance()
